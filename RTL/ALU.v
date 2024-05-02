c44e518b2130fe8658348a5c52e3f44b3df668cd
mskertem <me427@njit.edu>
2024-05-03 00:22:58 +0300

    bug düzeltmesi

--- RTL/ALU.v
+++ RTL/ALU.v
@@ -21,23 +21,23 @@
 
 
 module ALU(
-    input wire[6:0]funct7,
-    input wire[2:0]funct3,
-    input wire[31:0]A,B,
-    output wire[31:0]result,
-    output wire[3:0]alu_flags
+    input wire [6:0]funct7,
+    input wire [2:0]funct3,
+    input wire [31:0]A,B,
+    output wire [31:0]result,
+    output wire [3:0]alu_flags
     );
-    wire sub,cout,less_that_signed,less_that_unsigned,V,C,N,Z,b_src;
-    wire [31:0]adder_out;
+    wire sub,cout,less_that_signed,less_that_unsigned,V,C,N,Z;
+    wire [31:0]adder_out,shift_out,adder_b;
     reg  [31:0]res;
-    assign sub = funct7[5] | (funct3 == 3'b010)| (funct3 == 3'b011);
-    assign adder_b = sub ? ~adder_b : adder_b;
+    assign sub = funct7[5] | (funct3 == 3'b010) | (funct3 == 3'b011);
+    assign adder_b = sub ? ~B : B;
     assign V = (A[31]^adder_out[31]) & ~(A[31]^B[31]^sub);
     assign C = cout;
     assign N = adder_out[31];
     assign Z = (32'b0 == adder_out);
-    assign less_that_signed = N^V;
-    assign less_that_unsigned = ~cout;
+    assign less_than_signed = N^V;
+    assign less_than_unsigned = ~cout;
     
     assign alu_flags = {V,C,N,Z};
     
@@ -49,26 +49,22 @@ module ALU(
         .cout(cout)
     );
     
-    LeftShift lshigt(
-        .a(A),
-        .b(B[4:0]),
-        .y(left_shift_out)
-    );
-    RightShift rshift(
+    Shifter shifter (
         .a(A),
         .b(B[4:0]),
         .mode(funct7[5]),
-        .y(right_shift_out)
+        .sel(funct3[2]),
+        .y(shift_out)
     );
     
     always@(*) begin
         case(funct3)
             3'b000: res = adder_out;
-            3'b001: res = left_shift_out;
-            3'b010: res = {31'b0,less_that_signed};
-            3'b011: res = {31'b0,less_that_unsigned};
+            3'b001: res = shift_out;
+            3'b010: res = {31'b0,less_than_signed};
+            3'b011: res = {31'b0,less_than_unsigned};
             3'b100: res = A ^ B;
-            3'b101: res = right_shift_out;
+            3'b101: res = shift_out;
             3'b110: res =  A | B;
             3'b111: res =  A & B;
         endcase
