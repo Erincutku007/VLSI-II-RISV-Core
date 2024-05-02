506f904f04d362da909d5abf0d8d36ce57ea9147
mskertem <me427@njit.edu>
2024-05-03 00:22:25 +0300

    custom adder eklendi

--- RTL/Adder.v
+++ RTL/Adder.v
@@ -19,12 +19,30 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module Adder (input wire cin,
+              input [31:0] a, b,
+              output cout,
+              output [31:0] y);
 
-module Adder(
-        input cin,
-        input [31:0]a,b,
-        output [31:0]y,
-        output cout
-    );
-    assign {cout,y} = a+b+cin;
+wire [32:0] c;
+assign c[0] = cin;
+assign cout = c[32];
+
+generate
+  genvar i;
+  for (i = 0; i < 32; i = i + 1)
+    begin : generate_FA    
+        FA fa (.a(a[i]), .b(b[i]), .c_in(c[i]), .c_out(c[i+1]), .sum(y[i]));
+    end
+endgenerate 
+
 endmodule
+
+module FA (input a, b, c_in,
+           output c_out, sum);
+
+assign sum = a ^ b ^ c_in;
+assign c_out = (c_in & (a ^ b)) | (a & b);
+
+endmodule
+
