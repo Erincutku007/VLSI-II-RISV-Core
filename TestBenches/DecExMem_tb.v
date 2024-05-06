12cf6f9d0a492736b508d2f503a649986f9a36dc
Erincutku007 <36078723+Erincutku007@users.noreply.github.com>
2024-05-06 22:21:44 +0300

    testbenchler

---
+++ TestBenches/DecExMem_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/06/2024 10:04:52 PM
+// Design Name: 
+// Module Name: DecExMem_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DecExMem_tb(
+
+    );
+endmodule
