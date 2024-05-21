c118041a9f2fe1d1ce2606fa86dcc3e3c86ed669
mskertem <me427@njit.edu>
2024-05-21 21:52:24 +0300

    CLA is added

--- TestBenches/adder_tb.v
+++ TestBenches/adder_tb.v
@@ -5,28 +5,40 @@ module Adder_tb;
 
 	wire [31:0] s;
 	wire cout;
+    reg sub;
 	reg [31:0] x,y;
-	
-	
-	Adder uut(x,y,s,cout);
+		
+	Adder DUT (sub,x,y,cout,s);
         initial
         begin
-        $dumpfile("Adder_top.vcd");
-        $dumpvars(0, Adder_tb);
+        $dumpfile("CLA_TB.vcd");
+        $dumpvars(0, CLA_tb);
         end
   	initial
     	begin
-      	x = 32'h56745675; y = 32'h54546576; 
+      	x = 32'h56745675; y = 32'h54546576;
+        sub = 0;
 		#10
 		if((x+y) == {cout,s}) $display($time, " correct");
 		else $display("error");
-      	x = 32'hAB674594; y = 32'hAC784387; 
+
+      	x = 32'h92384923; y = 32'h;
+        sub = 0;
 		#10
 		if((x+y) == {cout,s}) $display($time, " correct");
+		else $display("error");
+
+      	x = 32'hAB674594; y = 32'hAC784387; 
+        sub = 1;
+		#10
+		if((x-y) == $signed(s)) $display($time, " correct");
+		else $display("error");
+
+      	x = 32'hAB674594; y = 32'hAC784387; 
+        sub = 1;
+		#10
+		if((x-y) == $signed(s)) $display($time, " correct");
 		else $display("error");
     	end
-  
-  	// initial
-    // 	$monitor(,$time,"x=%h,y=%h,s=%h,cout=%h",x,y,s,cout);
   
 endmodule
